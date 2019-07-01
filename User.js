25f1ebb48acda3849274f9e55f7141649d5169bc
M1NDX <luisfer.isc@gmail.com>
2019-06-30 23:37:31 -0500

    cambiar constructor, no recibir url

--- User.js
+++ User.js
@@ -1,13 +1,13 @@
 class User{
 
-    constructor(username, email, password, sexo='F', hobbies=[], url = ""){ 
+    constructor(username, email, password, sexo='F', hobbies=[]){ 
         this.id = ++User.count;
         this.username = username;
         this.email = email;
         this.password = password;
         this.hobbies = hobbies;
         this.sexo = sexo;
-        this.url = url;
+        this.url = ""; //"https://randomuser.me/api/portraits/men/65.jpg"
     }
 }
 
